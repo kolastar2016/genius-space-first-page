1ac7baadf47b9a41c7d58764af009377c5d36753
Микола Шуляк <mykolashuliak@MacBook-Pro-Mikola.local>
2023-11-10 12:12:23 +0200

    fix bug obout style and js

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpenSecond = document.querySelector('.modal-btn-open-second');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpenSecond.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
